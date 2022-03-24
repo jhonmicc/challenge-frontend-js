dc3e67199a9efa8a18b01812d4b1c012e50999dd
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-03-24 14:17:54 +0700

    add data and render it to the page

---
+++ challenge-03/binar-car-rental/controllers/pagesController.js
@@ -0,0 +1,28 @@
+exports.dashboard = (req, res) => {
+    const title = 'Dashboard';
+    const data = [{
+        id: 1,
+        user_email: 'tigernikson@gmail.com',
+        car: 'Fortuner 2022',
+        start_rent: 'Start Rent',
+        finish_rent: 'Finish Rent',
+        price: 1500000000,
+        status: 'On Process',
+    }, ];
+
+    const data2 = [{}];
+    res.render('dashboard', { title: title, dashboard: data });
+};
+
+exports.cars = (req, res) => {
+    const title = 'List Car';
+    const items = [{
+        id: 1,
+        img: '/images/car.png',
+        name: 'Nama/Tipe Mobil',
+        price: 430000,
+        rent: 'Start rent - Finish rent',
+        update: 'Updated at 4 Apr 2022, 09.00',
+    }, ];
+    res.render('cars', { title: title, cars: items });
+};
