053a2e3c1397935b1dfae6bdaf6871746ef5b103
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-03-24 14:21:24 +0700

    add pagesRouter

--- challenge-03/binar-car-rental/routes/index.js
+++ challenge-03/binar-car-rental/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var pagesRouter = require('./pages');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//     res.render('dashboard', { title: 'Dashboard' });
+// });
+
+router.use(pagesRouter);
 
 module.exports = router;
