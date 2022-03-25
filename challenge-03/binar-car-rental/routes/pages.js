3bda50cbd75881c7842cf1d6e811c56524031f25
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-03-25 18:14:10 +0700

    route the sign in page

--- challenge-03/binar-car-rental/routes/pages.js
+++ challenge-03/binar-car-rental/routes/pages.js
@@ -3,4 +3,5 @@ const page = require('../controllers/pagesController');
 
 router.get('/', page.dashboard);
 router.get('/cars', page.cars);
+router.get('/sign-in', page.sign_in);
 module.exports = router;
