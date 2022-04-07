d25b29fc86998ef918e60f51d975514bd46d349f
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-04-07 22:39:29 +0700

    arrange the components

--- challenge-04/binar-car-rental/src/App.js
+++ challenge-04/binar-car-rental/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import BinarCarRentalComponent from './Components';
+import DetailCar from './Components/DetailCar/DetailCar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BinarCarRentalComponent />} />
+        <Route path="/detail/:id" element={<DetailCar />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
