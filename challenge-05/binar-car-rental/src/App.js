34fc5a4c644e0bce11b1760772bb3319cc0a295d
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-04-19 00:30:05 +0700

    dispatch actions using useDispatch

--- challenge-05/binar-car-rental/src/App.js
+++ challenge-05/binar-car-rental/src/App.js
@@ -1,9 +1,27 @@
 // import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import BinarCarRentalComponent from './Components';
 import DetailCar from './Components/DetailCar/DetailCar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { carActions } from './Store/carSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3008/datas');
+        const datas = await response.data;
+        dispatch(carActions.setAllData(datas))
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Routes>
