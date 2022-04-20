79ca0d89bc83b480c3dee4af871dd218698e274a
Jhon Michael Manik <jhonmichaelmanik@gmail.com>
2022-04-20 15:55:32 +0700

    use createAsyncThunk

--- challenge-05/binar-car-rental/src/App.js
+++ challenge-05/binar-car-rental/src/App.js
@@ -5,31 +5,39 @@ import { useDispatch } from 'react-redux';
 import BinarCarRentalComponent from './Components';
 import DetailCar from './Components/DetailCar/DetailCar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { carActions } from './Store/carSlice';
+// import { carActions } from './Store/carSlice';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const CARS_URL = 'http://localhost:3008/datas'
+
+export const getData = createAsyncThunk('cars/getData', async () => {
+    try {
+        const response = await axios.get(CARS_URL);
+        const datas = await response.data;
+        // mendapatkan data untuk dimasukkan ke state terkini
+        // dispatch(carActions.setAllData(datas))
+        return datas
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get('http://localhost:3008/datas');
-        const datas = await response.data;
-        dispatch(carActions.setAllData(datas))
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getData();
-  }, [dispatch]);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<BinarCarRentalComponent />} />
-        <Route path="/detail/:id" element={<DetailCar />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getData())
+    }, [dispatch]);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<BinarCarRentalComponent />} />
+                <Route path="/detail/:id" element={<DetailCar />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
